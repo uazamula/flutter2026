6da027da251559806b269e391af6f5250848fcb8
uazamula <uazamula@ukr.net>
2026-01-03 20:24:31 +0200

    Властивості Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -5,29 +5,66 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  bool isDark=true;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: .fromSeed(
+        colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.deepPurple,
-          brightness: isDark?Brightness.dark:Brightness.light,
+          brightness: Brightness.dark,
         ),
       ),
-      home: WidgetTree(onBrightnessMode: (value){isDark=value; setState(() {
-
-      });},),
+      home: Scaffold(
+        resizeToAvoidBottomInset: true,
+        backgroundColor: Colors.teal,
+        appBar: AppBar(title: Text('Flutter Mapp')),
+        drawer: SafeArea(
+          child: Drawer(
+            child: Column(
+              children: [
+                //  DrawerHeader(child: Text('Drawer')),
+                ListTile(title: Text('data1')),
+                ListTile(title: Text('data2')),
+              ],
+            ),
+          ),
+        ),
+        floatingActionButton: Column(
+          mainAxisSize: .min,
+          children: [
+            FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
+            SizedBox(height: 10),
+            FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
+          ],
+        ),
+        bottomNavigationBar: NavigationBar(
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+            NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
+          ],
+          onDestinationSelected: (value) {},
+          selectedIndex: 1,
+        ),
+        bottomSheet: SafeArea(
+          child: Container(
+            padding: const EdgeInsets.all(16),
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: const Text('Підтвердити'),
+            ),
+          ),
+        ),
+        body: ListView.builder(
+          itemCount: 50,
+          itemBuilder: (_, i) => ListTile(title: Text('Item $i')),
+        ),
+      ),
     );
   }
 }
